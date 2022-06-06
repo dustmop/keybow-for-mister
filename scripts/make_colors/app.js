7b1f9a9b18d40749e0d79f0fcf1fd013d0d03d42
Dustin Long <me@dustmop.io>
2022-06-06 14:56:46 -0400

    Remove dead, unused files

--- scripts/make_colors/app.js
+++
@@ -1,8 +0,0 @@
-const ra = require('raster');
-
-let colors = ra.mixColors([ 0, 0xff0000,
-                           16, 0xff8800,
-                           32, 0xffff00,
-                           48, 0x88ff00,
-                           64, 0x880000]);
-console.log(colors);
